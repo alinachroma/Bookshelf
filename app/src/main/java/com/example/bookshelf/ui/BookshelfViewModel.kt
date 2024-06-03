839ec424d104f9ba933dc772b8ad3796d2456561
achroma <alinachroma@gmail.com>
2024-06-03 19:04:25 +0200

    add ViewModel

--- app/src/main/java/com/example/bookshelf/ui/BookshelfViewModel.kt
+++ app/src/main/java/com/example/bookshelf/ui/BookshelfViewModel.kt
@@ -1,5 +1,25 @@
+package com.example.bookshelf.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
 sealed interface BookshelfUiState {
     data class Success(val result: String) : BookshelfUiState
     object Loading : BookshelfUiState
     object Error : BookshelfUiState
+}
+
+class BookshelfViewModel : ViewModel() {
+    var uiState: BookshelfUiState by mutableStateOf(BookshelfUiState.Loading)
+        private set
+
+    init {
+        getBooksData()
+    }
+
+    fun getBooksData() {
+        TODO()
+    }
 }
