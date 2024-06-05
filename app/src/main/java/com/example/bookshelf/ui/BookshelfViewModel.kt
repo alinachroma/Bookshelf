3802189f5a2736712fb3babf2f6496233d4d3352
achroma <alinachroma@gmail.com>
2024-06-05 16:00:20 +0200

    add needed imports

--- app/src/main/java/com/example/bookshelf/ui/BookshelfViewModel.kt
+++ app/src/main/java/com/example/bookshelf/ui/BookshelfViewModel.kt
@@ -4,6 +4,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.bookshelf.BookshelfApplication
+import com.example.bookshelf.data.BookshelfRepository
+import com.example.bookshelf.model.BooksList
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed interface BookshelfUiState {
     data class Success(val result: BooksList) : BookshelfUiState
