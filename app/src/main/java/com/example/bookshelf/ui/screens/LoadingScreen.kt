56e926b41cc366a9cf881c562bedc10b119bc6b9
achroma <alinachroma@gmail.com>
2024-06-05 19:46:04 +0200

    refactor Loading Screen

--- app/src/main/java/com/example/bookshelf/ui/screens/LoadingScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/LoadingScreen.kt
@@ -1,19 +1,41 @@
 package com.example.bookshelf.ui.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.bookshelf.ui.BookshelfUiState
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.bookshelf.R
+import com.example.bookshelf.ui.theme.BookshelfTheme
+import com.example.bookshelf.utils.ThemePreviews
 
 @Composable
 fun LoadingScreen(
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier.fillMaxSize()
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "LOADING")
+        Image(
+            modifier = modifier.size(200.dp),
+            painter = painterResource(R.drawable.loading_img),
+            contentDescription = stringResource(R.string.loading)
+        )
+    }
+}
+
+@ThemePreviews
+@Composable
+fun LoadingScreenPreviews() {
+    BookshelfTheme {
+        LoadingScreen()
     }
 }
