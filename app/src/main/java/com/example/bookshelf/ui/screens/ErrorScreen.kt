b94b4dc61215fdd31a6c881ac47cb6189dd3bd64
achroma <alinachroma@gmail.com>
2024-06-05 19:46:13 +0200

    refactor Error Screen

--- app/src/main/java/com/example/bookshelf/ui/screens/ErrorScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/ErrorScreen.kt
@@ -1,18 +1,46 @@
 package com.example.bookshelf.ui.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import com.example.bookshelf.R
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.bookshelf.ui.theme.BookshelfTheme
+import com.example.bookshelf.utils.ThemePreviews
 
 @Composable
 fun ErrorScreen(
+    retryAction: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier.fillMaxSize()
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "ERROR")
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.error), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+fun ErrorScreenPreviews() {
+    BookshelfTheme {
+        ErrorScreen(retryAction = {})
     }
 }
