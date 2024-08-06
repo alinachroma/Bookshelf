51a2791c8e6d570cbf8af9f2ae60f6af78723172
achroma <alinachroma@gmail.com>
2024-08-06 13:21:27 +0200

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlinx.serialization)
+    id("com.google.devtools.ksp")
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -54,22 +56,41 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // ViewModel utilities for Compose
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.gson.converter)
+
+    // Retrofit (REST client utilities)
     implementation(libs.retrofit)
+
+    // An image loading library for Android backed by Kotlin Coroutines
     implementation(libs.coil)
+
+    // Compiler plugin for serializing
     implementation(libs.kotlinx.serialization)
     implementation(libs.kotlinx.serialization.converter)
-    implementation(libs.gson.converter)
+
+    // HTTP & HTTP/2 client for Android
     implementation(libs.okhttp)
 
+    // Room & database
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+
     testImplementation(libs.junit)
+
+    // For testing coroutines
     testImplementation(libs.kotlinx.coroutines.test)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
